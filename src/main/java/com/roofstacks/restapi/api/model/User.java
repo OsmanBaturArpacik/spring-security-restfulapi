27b53bc61b47e90406737c1a7d3157870e3b06ee
osman batur <osman.arpacik@roofstacks.com>
2024-07-19 22:30:19 +0300

    sign in, sign up files created

--- src/main/java/com/roofstacks/restapi/api/model/User.java
+++ src/main/java/com/roofstacks/restapi/api/model/User.java
@@ -1,19 +1,8 @@
 package com.roofstacks.restapi.api.model;
 
-import com.roofstacks.restapi.service.UserService;
 import lombok.Data;
-import org.springframework.beans.factory.annotation.Autowired;
 
 @Data
 public class User {
-    private static int lastId;
-    private int id;
-    private String name;
-    private int roleId;
-    public User(String name, int roleId) {
-        this.id = ++lastId;
-        this.name = name;
-        this.roleId = roleId;
-    }
 
 }
---
+++ src/main/java/com/roofstacks/restapi/service/SignUpService.java
@@ -0,0 +1,4 @@
+package com.roofstacks.restapi.service;
+
+public class SignUpService {
+}
