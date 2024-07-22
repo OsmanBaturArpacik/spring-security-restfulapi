a3c15e90223ad367d9f5c8b59a97a2c0343c358e
osman batur <osman.arpacik@roofstacks.com>
2024-07-22 11:51:28 +0300

    tokenized security, certs RSAkeys, serilazed user list added

--- src/main/java/com/roofstacks/restapi/RestApiApplication.java
+++ src/main/java/com/roofstacks/restapi/RestApiApplication.java
@@ -1,8 +1,11 @@
 package com.roofstacks.restapi;
 
+import com.roofstacks.restapi.api.config.RsaKeyProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+@EnableConfigurationProperties(RsaKeyProperties.class)
 @SpringBootApplication
 public class RestApiApplication {
 
